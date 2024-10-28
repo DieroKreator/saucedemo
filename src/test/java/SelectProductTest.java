42ee5df7b7e51e50af7eccdd50779a8456169729
DieroKreator <qtechwired@gmail.com>
2024-10-28 17:46:14 -0300

    Remove unused line of code

--- src/test/java/SelectProductTest.java
+++ src/test/java/SelectProductTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,7 +15,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class SelectProductTest {
 
   private WebDriver driver;
-  JavascriptExecutor js;
 
   @BeforeEach
   public void setUp() {
